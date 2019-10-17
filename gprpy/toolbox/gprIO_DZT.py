f9d862a1befb0fdc045574118a2558c77f39f25c
kwinkunks <matt@agilegeoscience.com>
2019-10-17 11:54:14 +0100

    reading arbitrary traces

--- gprpy/toolbox/gprIO_DZT.py
+++ gprpy/toolbox/gprIO_DZT.py
@@ -1,28 +1,25 @@
 import struct
 import numpy as np
-#import re # Regular expressions
 
-def readdzt(filename):
+
+def readdzt(filename, start_scan=0, n_scans=-1):
     '''
-    Reads a GSSI .DZT data file. 
+    Reads a GSSI .DZT data file.
 
-    INPUT: 
-    filename     data file name including .DZT extension
+    Args:
+        filename (str): Data file name including .DZT extension.
+        start_scan (int): First scan to read, by index. Default is 0 (read from
+            start of data.)
+        n_scans (int): Number of scan traces to read. Default is -1 (read all).
 
-    OUTPUT:
-    data          data matrix whose columns contain the traces
-    info          dict with information from the header
+    Returns:
+        ndarray: data matrix whose columns contain the traces.
+        dict: dict with information from the header.
 
     Thanks to Ian Nesbitt for pointing out extended headers and
-    providing the documentation file.
+    providing the documentation file. Documentation file is
+    DZT.File.Format.6-14-16.pdf
     '''
-
-    # Documentation file is DZT.File.Format.6-14-16.pdf
-    
-    info = {}
-    
-    fid = open(filename,'rb');
-
 
     # H is unsigned int 16 (ushort = uint16)
     # h is short (int16)
@@ -30,117 +27,118 @@ def readdzt(filename):
     # i is int32
     # f is float
 
-    # All of the following information is from DZT.File.Format.6-14-16.pdf
-    # Provided by Ian Nesbitt
-    
     minheadsize = 1024
-    infoareasize = 128
-    
-    rh_tag = struct.unpack('h', fid.read(2))[0]  # Pos 00
-    
-    # Size of the header
-    rh_data = struct.unpack('h', fid.read(2))[0] # Pos 02
-    
-    # Samples per trace
-    rh_nsamp = struct.unpack('h', fid.read(2))[0] # Pos 04
-    info["rh_nsamp"] = rh_nsamp
 
-    # Bits per word
-    rh_bits = struct.unpack('h', fid.read(2))[0] # Pos 06
-    
-    # Binary offset
-    rh_zero = struct.unpack('h', fid.read(2))[0] # Pos 08
+    info = {}
 
-    # Scans per second
-    rhf_sps = struct.unpack('f', fid.read(4))[0] # Pos 10
-    info["rhf_sps"] = rhf_sps
-    
-    # Scans per meter
-    rhf_spm = struct.unpack('f', fid.read(4))[0] # Pos 14
-    info["rhf_spm"] = rhf_spm
+    with open(filename, 'rb') as fid:
+        info["rh_tag"] = struct.unpack('h', fid.read(2))[0]  # Pos 00
 
-    # Meters per mark
-    rhf_mpm = struct.unpack('f', fid.read(4))[0] # Pos 18
+        # Size of the header
+        info["rh_data"] = struct.unpack('h', fid.read(2))[0]  # Pos 02
 
-    # Startposition [ns]
-    rhf_position = struct.unpack('f', fid.read(4))[0] # Pos 22
-    info["rhf_position"] = rhf_position
+        # Samples per trace
+        info["rh_nsamp"] = struct.unpack('h', fid.read(2))[0]  # Pos 04
 
-    # length of trace [ns]
-    rhf_range = struct.unpack('f', fid.read(4))[0] # Pos 26
-    info["rhf_range"] = rhf_range
+        # Bits per word
+        info["rh_bits"] = struct.unpack('h', fid.read(2))[0]  # Pos 06
 
-    # Number of passes
-    rh_npass = struct.unpack('h', fid.read(2))[0] # Pos 30
-    info["rh_npass"] =  rh_npass
+        # Binary offset
+        info["rh_zero"] = struct.unpack('h', fid.read(2))[0]  # Pos 08
 
-    # Creation date and time
-    rhb_cdt = struct.unpack('f', fid.read(4))[0] # Pos 32
-    info["rhb_cdt"] = rhb_cdt
-    
-    # Last modified date & time
-    rhb_mdt = struct.unpack('f', fid.read(4))[0]  # Pos 36
-    
-    # no idea
-    rh_mapOffset = struct.unpack('h', fid.read(2))[0] # Pos 40
+        # Scans per second
+        info["rhf_sps"] = struct.unpack('f', fid.read(4))[0]  # Pos 10
 
-    # No idea
-    rh_mapSize = struct.unpack('h',fid.read(2))[0] # Pos 42
+        # Scans per meter
+        info["rhf_spm"] = struct.unpack('f', fid.read(4))[0]  # Pos 14
 
-    # offset to text
-    rh_text = struct.unpack('h',fid.read(2))[0] # Pos 44
+        # Meters per mark
+        info["rhf_mpm"] = struct.unpack('f', fid.read(4))[0]  # Pos 18
 
-    # Size of text
-    rh_ntext = struct.unpack('h',fid.read(2))[0] # Pos 46
+        # Startposition [ns]
+        info["rhf_position"] = struct.unpack('f', fid.read(4))[0]  # Pos 22
 
-    # offset to processing history
-    rh_proc = struct.unpack('h',fid.read(2))[0] # Pos 48
+        # length of trace [ns]
+        info["rhf_range"] = struct.unpack('f', fid.read(4))[0]  # Pos 26
 
-    # size of processing history
-    rh_nproc = struct.unpack('h',fid.read(2))[0] # Pos 50
+        # Number of passes
+        info["rh_npass"] = struct.unpack('h', fid.read(2))[0]  # Pos 30
 
-    # number of channels
-    rh_nchan = struct.unpack('h',fid.read(2))[0] # Pos 52
+        # Creation date and time
+        info["rhb_cdt"] = struct.unpack('f', fid.read(4))[0]  # Pos 32
 
-    # ... and more stuff we don't really need
+        # Last modified date & time
+        info["rhb_mdt"] = struct.unpack('f', fid.read(4))[0]  # Pos 36
 
-    fid.close()
+        # no idea
+        info["rh_mapOffset"] = struct.unpack('h', fid.read(2))[0]  # Pos 40
 
-    # offset will tell us how long the header is in total
-    # There could be a minimal header of 1024 bits
-    # (very old files may have had 512 bits)
-    if rh_data < minheadsize:
-        offset = minheadsize*rh_data
-    else:
-        offset = minheadsize*rh_nchan   
+        # No idea
+        info["rh_mapSize"] = struct.unpack('h', fid.read(2))[0]  # Pos 42
 
-    # Define data type based on words per bit
+        # offset to text
+        info["rh_text"] = struct.unpack('h', fid.read(2))[0]  # Pos 44
+
+        # Size of text
+        info["rh_ntext"] = struct.unpack('h',  fid.read(2))[0]  # Pos 46
+
+        # offset to processing history
+        info["rh_proc"] = struct.unpack('h', fid.read(2))[0]  # Pos 48
+
+        # size of processing history
+        info["rh_nproc"] = struct.unpack('h', fid.read(2))[0]  # Pos 50
+
+        # number of channels
+        info["rh_nchan"] = struct.unpack('h', fid.read(2))[0]  # Pos 52
+
+        # ... and more stuff we don't really need.
+
+    # Define data type based on words per bit.
     # From documentation: Eight byte and sixteen byte samples are
     # unsigned integers. Thirty-two bit samples are signed integers.
-    if rh_bits == 8:
-        datatype = 'uint8' # unsigned char
-    elif rh_bits == 16:
-        datatype = 'uint16' # unsigned int
-    elif rh_bits == 32:
-        datatype = 'int32'
+    if info["rh_bits"] == 8:
+        datatype = np.uint8  # unsigned int
+    elif info["rh_bits"] == 16:
+        datatype = np.uint16  # unsigned int
+    elif info["rh_bits"] == 32:
+        datatype = np.int32
 
-        
-    # Read the entire file
-    vec = np.fromfile(filename,dtype=datatype)
-        
-    headlength = offset/(rh_bits/8)
+    # Offset will tell us how long the header is in total.
+    # There could be a minimal header of 1024 bits, or
+    # very old files may have had 512 bits.
+    if info["rh_data"] < minheadsize:
+        head_offset = minheadsize * info["rh_data"]
+    else:
+        head_offset = minheadsize * info["rh_nchan"]
 
-    # Only use the data, discard the header
-    datvec = vec[int(headlength):]
-    
+    # Deal with offset
+    try:
+        start_offset = int(start_scan * info['rh_nchan'] * info['rh_nsamp'] * info['rh_bits'] / 8)
+    except:
+        warnings.warn("Unable to set start_offset from start_scan.")
+        start_offset = 0
+    if n_scans > 0:
+        try:
+            count = int(n_scans * info['rh_nsamp'] * info['rh_nchan'])
+        except:
+            warnings.warn("Unable to set count from n_scans.")
+            count = -1
+    else:
+        count = -1
+
+    # Read the file
+    datvec = np.fromfile(filename,
+                         dtype=datatype,
+                         offset=head_offset + start_offset,
+                         count=count,
+                         )
+
     # Turn unsigned integers into signed integers
     # Only necessary where unsigned
-    if rh_bits == 8 or rh_bits == 16:
-        datvec = datvec - (2**rh_bits)/2.0
+    if info["rh_bits"] == 8 or info["rh_bits"] == 16:
+        datvec = datvec.astype(np.int32) - 2**info["rh_bits"] // 2
 
-    # reshape into matrix
-    data = np.reshape(datvec,[int(len(datvec)/rh_nsamp),rh_nsamp])
+    # Reshape into matrix
+    data = datvec.reshape(-1, info["rh_nsamp"]*info["rh_nchan"]).T
 
-    data = np.asmatrix(data)
-    
-    return data.transpose(), info
+    return data, info
